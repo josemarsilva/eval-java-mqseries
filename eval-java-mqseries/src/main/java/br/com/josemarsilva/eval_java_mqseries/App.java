d81dbfc240c7ab447338e05297d11ad6b22d9099
josemarsilva <josemar.furegatti@gmail.com>
2020-02-12 22:32:36 -0300

    .

---
+++ eval-java-mqseries/src/main/java/br/com/josemarsilva/eval_java_mqseries/App.java
@@ -0,0 +1,13 @@
+package br.com.josemarsilva.eval_java_mqseries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
