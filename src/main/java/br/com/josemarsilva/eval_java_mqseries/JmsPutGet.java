67ce154e4e5a306dbaae77130137087c41487986
josemarsilva <josemar.furegatti@gmail.com>
2020-03-01 17:01:23 -0300

    .

--- src/main/java/br/com/josemarsilva/eval_java_mqseries/JmsPutGet.java
+++ src/main/java/br/com/josemarsilva/eval_java_mqseries/JmsPutGet.java
@@ -41,7 +41,7 @@ public class JmsPutGet {
 	private static final int PORT = 1414; // Listener port for your queue manager
 	private static final String CHANNEL = "DEV.APP.SVRCONN"; // Channel name
 	private static final String QMGR = "QM1"; // Queue manager name
-	private static final String APP_USER = "admin"; // User name that application uses to connect to MQ
+	private static final String APP_USER = ""; // User name that application uses to connect to MQ
 	private static final String APP_PASSWORD = "passw0rd"; // Password that the application uses to connect to MQ
 	private static final String QUEUE_NAME = "DEV.QUEUE.1"; // Queue that the application uses to put and get messages to and from
 
